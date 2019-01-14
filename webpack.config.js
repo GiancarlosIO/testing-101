4a3c32deeb02b78251c9e9d30e07ee2a57988843
Giancarlos <giancarlos.isasi@gmail.com>
2019-01-13 20:38:34 -0500

    Configure publicPath in webpack to work with gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/dist",
+    publicPath: "/testing-101/",
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
